f1f909e8486b555580407978e694c7cea0ba7c6b
Kody Conrad <kody.n.conrad@gmail.com>
2024-05-09 11:18:32 -0700

    Updating `main` for better indication of successful flashing.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
 #include "mbed.h"
 
-int main()
-{
-	while(true) 
-	{
-		printf("Hello world from Mbed CE!\n");
-		ThisThread::sleep_for(1s);
-	}
+UnbufferedSerial pc(CONSOLE_TX, CONSOLE_RX);
+PwmOut pwm(LED1);
 
-	// main() is expected to loop forever.
-	// If main() actually returns the processor will halt
-	return 0;
+int main() {
+    printf("Mbed OS %d.%d.%d\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+
+    // Set the PWM frequency and duty cycle
+    pwm.period(0.01); // 100 Hz frequency
+    pwm.write(0.5);   // 50% duty cycle
+
+    std::chrono::milliseconds duration(100);
+
+    while (1) {
+        // Increase duty cycle to 75%
+        for (float i = 0.0; i < .90; i += 0.1)
+        {
+            pwm.write(i);
+            ThisThread::sleep_for(duration);
+        }
+
+        for (float i = .90; i > 0.0; i -= 0.1)
+        {
+            pwm.write(i);
+            ThisThread::sleep_for(duration);
+        }
+    }
 }

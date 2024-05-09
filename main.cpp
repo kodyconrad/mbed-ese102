aadcf03d6e6d616ade77d16e1ad26a9e9b732ed0
Kody Conrad <kody.n.conrad@gmail.com>
2024-05-09 11:19:02 -0700

    Comment updates.

--- main.cpp
+++ main.cpp
@@ -6,20 +6,21 @@ PwmOut pwm(LED1);
 int main() {
     printf("Mbed OS %d.%d.%d\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
 
-    // Set the PWM frequency and duty cycle
-    pwm.period(0.01); // 100 Hz frequency
-    pwm.write(0.5);   // 50% duty cycle
+    // Set intial PWM frequency and duty cycle
+    pwm.period(0.01);
+    pwm.write(0.0);
 
     std::chrono::milliseconds duration(100);
 
     while (1) {
-        // Increase duty cycle to 75%
+        // Increase duty cycle to 90%
         for (float i = 0.0; i < .90; i += 0.1)
         {
             pwm.write(i);
             ThisThread::sleep_for(duration);
         }
 
+        // Decrease duty cycle to 0%
         for (float i = .90; i > 0.0; i -= 0.1)
         {
             pwm.write(i);

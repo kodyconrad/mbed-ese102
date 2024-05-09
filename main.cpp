6f338055d038b7d51ceb543ed72b1044dd056d62
Kody Conrad <kody.n.conrad@gmail.com>
2024-05-09 13:36:16 -0700

    Pulse smoothing.

--- main.cpp
+++ main.cpp
@@ -10,18 +10,19 @@ int main() {
     pwm.period(0.01);
     pwm.write(0.0);
 
-    std::chrono::milliseconds duration(100);
+    float pulse_step = 0.05;
+    std::chrono::milliseconds duration(50);
 
     while (1) {
         // Increase duty cycle to 90%
-        for (float i = 0.0; i < .90; i += 0.1)
+        for (float i = 0.0; i < .90; i += pulse_step)
         {
             pwm.write(i);
             ThisThread::sleep_for(duration);
         }
 
         // Decrease duty cycle to 0%
-        for (float i = .90; i > 0.0; i -= 0.1)
+        for (float i = .90; i > 0.0; i -= pulse_step)
         {
             pwm.write(i);
             ThisThread::sleep_for(duration);

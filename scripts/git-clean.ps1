3bb16ef4c839db729eb40215814e81978bcb6077
Kody Conrad <kody.n.conrad@gmail.com>
2024-05-10 13:12:47 -0700

    Removal of settings.json for vscode and introduction of scripts to help project cleanup.

---
+++ scripts/git-clean.ps1
@@ -0,0 +1,12 @@
+# Navigate to the parent directory of the script, which is the root of your git project
+Set-Location -Path (Join-Path -Path $PSScriptRoot -ChildPath "..")
+
+# Reset the head of the project
+git reset --hard HEAD
+
+# Clean up all files not in the head of the project
+# The 'clean' command removes untracked files from your working directory
+# The '-d' option tells it to remove untracked directories as well
+# The '-x' option tells it to also remove files ignored by git
+# The '-e !mbed-os/' option tells it to exclude the 'mbed-os' directory
+git clean -d -x -e '!mbed-os/'

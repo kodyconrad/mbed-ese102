64ca891cf70ee65c5713472e1c59288ed01327e0
Kody Conrad <kody.n.conrad@gmail.com>
2024-06-18 07:53:12 -0700

    Initial Mutex experimentation for module 4.

---
+++ module-4/Src/main.cpp
@@ -0,0 +1,29 @@
+/* Mutex Experimentation
+
+*/
+#include "mbed.h"
+
+
+Mutex stdio_mutex;
+Thread t2, t3;
+
+void notify(const char *name, int state) {
+    stdio_mutex.lock();
+    printf("%s: %d\n\r", name, state);
+    stdio_mutex.unlock();
+}
+
+void test_thread(void const *args) {
+    while (true){
+        notify((const char *)args, 0);
+        ThisThread::sleep_for(1000ms);
+        notify((const char *)args, 1);
+        ThisThread::sleep_for(1000ms);
+    }
+}
+
+int main() {
+    t2.start(callback(test_thread, (void *)"Th 2"));
+    t3.start(callback(test_thread, (void *)"Th 3"));
+    test_thread((void *)"Th 1");
+}
